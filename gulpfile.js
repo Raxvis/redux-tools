18d899029630b9772e2b4781dbb3dffc893f7d6c
William <william.h.giles@gmail.com>
2018-08-08 12:45:20 -0500

    update build system

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@ const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 const eslintIfFixed = require('gulp-eslint-if-fixed');
 const gulp = require('gulp');
-const through = require('through2');
 const gulpif = require('gulp-if');
-const sourcemaps = require('gulp-sourcemaps');
 
 const isJS = (file) => file.path.split('.').pop() === 'js';
 
@@ -14,17 +12,8 @@ gulp.task('build', () =>
 		.pipe(gulpif(isJS, eslint({ fix: true })))
 		.pipe(eslint.format())
 		.pipe(gulpif(isJS, eslintIfFixed('src')))
-		.pipe(sourcemaps.init())
 		.pipe(babel())
-		.pipe(
-			through.obj((file, enc, callback) => {
-				file.originalPath = file.path;
-				file.path = file.path.replace('src', 'dist');
-				callback(null, file);
-			}),
-		)
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('packages')),
+		.pipe(gulp.dest('dist')),
 );
 gulp.task('default', ['build']);
 gulp.task('watch', () => {
