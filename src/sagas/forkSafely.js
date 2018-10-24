8a53c179fb123f85087373fcd3f3258446f7d2f0
William <william@prefinem.com>
2018-10-24 17:36:11 -0500

    fixes #1

--- src/sagas/forkSafely.js
+++ src/sagas/forkSafely.js
@@ -1,14 +1,15 @@
-import { fork } from 'redux-saga/effects';
+import { fork, takeEvery, takeLatest, throttle } from 'redux-saga/effects';
+import takeLatestPerKey from './takeLatestPerKey';
 import wrapSagaSafely from './wrapSagaSafely';
 
-const sagaHelpers = ['takeEvery', 'takeLatest', 'takeLatestPerKey'];
+const sagaHelpers = [takeEvery, takeLatest, takeLatestPerKey];
 
 const forkSafely = (sagaHelper, ...args) => {
-	if (sagaHelper.name === 'throttle') {
+	if (sagaHelper === throttle) {
 		const [ms, fn, pattern, saga, ...rest] = args;
 
 		return fork(sagaHelper, ms, fn, pattern, wrapSagaSafely(saga), ...rest);
-	} else if (sagaHelpers.includes(sagaHelper.name)) {
+	} else if (sagaHelpers.includes(sagaHelper)) {
 		const [pattern, saga, ...rest] = args;
 
 		return fork(sagaHelper, pattern, wrapSagaSafely(saga), ...rest);
